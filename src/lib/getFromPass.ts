9f035a18ff03aea6cb55e993fa1d17b704869fee
Jakob Goebel <jakob.goebel@avenga.com>
2021-02-03 14:35:50 +0100

    fix infinity loop bug when pass entry could not be found

--- src/lib/getFromPass.ts
+++ src/lib/getFromPass.ts
@@ -3,16 +3,25 @@ import {promisify} from 'util'
 
 const exec = promisify(execCb)
 
+const _cache: Record<string, string> = {}
+
 export async function getFromPass(name: string, line: number): Promise<string> {
-  return await tryGetFromPass(name, line)
+  const cacheKey = `${name}_${line}`
+  if (_cache[cacheKey]) {
+    return _cache[cacheKey]
+  }
+
+  const result = await tryGetFromPass(name, line)
+  _cache[cacheKey] = result
+
+  return result
 }
 
 async function tryGetFromPass(name: string, line: number): Promise<string> {
   let output: string = ''
   let result: {stdout: string; stderr: string} | null = null
-  const tries = 10
 
-  while (!output && tries > 0) {
+  for (let tries = 0; !output && tries < 30; tries++) {
     result = await exec(`pass ${name} | sed -n ${line}p`)
     output = result.stdout.trim()
   }
